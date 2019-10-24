57617c2b18abd37279942e5be7e52958fb88504c
anabahena <ana-bahena@live.com.mx>
2019-10-24 17:43:06 -0500

    cambiando menu hamburguesa

---
+++ main.js
@@ -0,0 +1,18 @@
+// const button = document.getElementById('button-nav');
+
+
+// const menuHamburguer = () =>{
+//     const menu = document.getElementById('nav-opt')
+//     if(menu.classList.contains('disabled-menu')){
+//         console.log('mostrar');   
+//         menu.classList.remove('disabled-menu');
+//         menu.classList.add('enabled-menu');
+//     }else{
+//         menu.classList.remove('enabled-menu');
+//         menu.classList.add('disabled-menu')
+//         console.log('ocultar');
+//     }
+//     console.log('apretando botón');
+// }
+
+// button.addEventListener('click', menuHamburguer)
